e6446b82fb2f82b537a9340c9640f185a272f4d1
Aarushi <aaru.lakhi@gmail.com>
2024-11-02 23:05:00 -0500

    gallery cards refactored. fonts added.

--- frontend/screens/GalleryComponents/CompactCard.js
+++ frontend/screens/GalleryComponents/CompactCard.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+      'SourceCodePro-Regular': require('../.././assets/fonts/SourceCodePro-Regular.ttf'),
+      'SourceCodePro-Medium': require('../.././assets/fonts/SourceCodePro-Medium.ttf'),
+      'SourceCodePro-Italic': require('../.././assets/fonts/SourceCodePro-Italic.ttf'),
+    });
+  };
 
 const CompactCard = ({ image, title, subtitle }) => {
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    useEffect(() => {
+      fetchFonts()
+        .then(() => {
+          setFontLoaded(true);
+        })
+        .catch((error) => console.error(error));
+    }, []);
+  
+    if (!fontLoaded) {
+      return <AppLoading />;
+    }
+
+
   return (
     <View style={styles.card}>
       <Image source={{ uri: image }} style={styles.image} />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.subtitle}>{subtitle}</Text>
+      <Text style={[styles.title, { fontFamily: 'SourceCodePro-Medium' }]}>{title}</Text>
+      <Text style={[styles.subtitle, { fontFamily: 'SourceCodePro-Italic' }]}>{subtitle}</Text>
     </View>
   );
 };
@@ -27,11 +52,13 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     padding: 5,
+    paddingBottom: 0,
   },
   subtitle: {
     fontStyle: 'italic',
     color: 'gray',
     padding: 5,
+    paddingTop: 3,
   },
 });
 
